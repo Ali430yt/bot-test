b6dc8619cf03928b6ab7dc15b798543724ca67b8
Ali430yt <69361270+Ali430yt@users.noreply.github.com>
2022-04-11 10:21:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -105,5 +105,5 @@ def redirect_message():
 
 if __name__ == "__main__":
     bot.remove_webhook()
-    bot.set_webhook(url=str(APP_URL))
+    bot.set_webhook(url="https://bot-user-insta.herokuapp.com/"+str(BOT_TOKEN))
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
